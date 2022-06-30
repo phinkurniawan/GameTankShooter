173bc5fff859e061eab3f77b97d2984613cc0ecf
2072017EvanAgustianLukius <71422920+2072017EvanAgustianLukius@users.noreply.github.com>
2022-06-30 21:50:10 +0700

    commit

--- joyStick.gd
+++ joyStick.gd
@@ -1,7 +1,19 @@
-extends Area2D
+extends Control
 
+var start_pos: Vector2 = Vector2.ZERO
+var end_pos: Vector2 = Vector2.ZERO
+var valid_pos = false
+onready var js_pos = get_node("Background").react_position
+onready var js_bag = get_node("Background")
+onready var js_handle = get_node("Background/bottom")
 
-onready var big_circle = $Circle
-onready var small_circle = $Circle/SmallCircle
-onready var max_distance = $CollisionShape2D.shape.radius
+func _input(event: InputEvent) -> void:
+	if valid_pos:
+		if event is InputEventScreenDrag:
+			if start_pos == Vector2.ZERO:
+				start_pos = event.position
+				js_bag.react_position = start_pos
+				
 
+func _on_TextureButton_button_down():
+	valid_pos = true
